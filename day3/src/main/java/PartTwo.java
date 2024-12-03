e5bf229b341b79937af8e6d95018f6691aa97b33
Eoin Treacy <eointreacy5@gmail.com>
2024-12-03 10:40:59 +0000

    day 3 part 2

--- day3/src/main/java/PartTwo.java
+++ day3/src/main/java/PartTwo.java
@@ -1,9 +1,57 @@
-
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PartTwo {
+    public static boolean active = true;
 
+    public static int main(char[][] input) {
+        return Arrays.stream(input)
+                        .map(line -> extract(line))
+                        .mapToInt(line -> multiply(line))
+                        .sum();
+    }
+
+    public static ArrayList<String> extract(char[] line) {
+
+        ArrayList<String> cleaned = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\\'t\\(\\)");
+        Matcher matcher = pattern.matcher(new String(line));
+        
+        while(matcher.find()) {
+            cleaned.add(matcher.group());
+        }
+        System.out.println(cleaned);
+
+        return cleaned;
+    }
+
+    public static int multiply (ArrayList<String> strings) {
+        int result = 0;
+        for (String s: strings) {
+            if (s.equals("don't()")) {
+                active = false;
+                continue;
+            }
+
+            if (s.equals("do()")) {
+                active = true;
+            }
+
+            if (active && !s.equals("do()") && !s.equals("don't()")) {
+                // split it on the ','
+                String[] halves = s.split(",");
+                // remove everything that isn't a number
+                String left = halves[0].replaceAll("[^0-9]", "");
+                String right = halves[1].replaceAll("[^0-9]", "");
+                // multiply each number
+                result += (Integer.parseInt(left) * Integer.parseInt(right));
+    
+            }
+       }
+
+        return result;
+    }
 }
-
-
--- day3/src/test/java/PartTwoTest.java
+++ day3/src/test/java/PartTwoTest.java
@@ -5,23 +5,25 @@ import org.junit.jupiter.api.Test;
 import core.InputReader;
 
 public class PartTwoTest {
-    // @Test
-    // public void testSample() {
-    //     try {
-    //         int[][] input = InputReader.readInputAsInt("src/test/resources/sample.txt");
-    //         assertEquals(PartTwo.main(input), 4);
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-    
-    // @Test
-    // public void testInput() {
-    //     try {
-    //         int[][] input = InputReader.readInputAsInt("src/test/resources/input.txt");
-    //         System.out.println(PartTwo.main(input));
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+
+    @Test
+    public void testInputReader () {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/sample.txt");
+            assertEquals(PartTwo.main(input), 48);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInput() {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/input.txt");
+            System.out.println(PartTwo.main(input));
+            // 93994894 too high
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
