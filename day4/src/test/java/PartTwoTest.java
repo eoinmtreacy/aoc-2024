e5523b9eaf56bfe881bec28df3f51f191e71deee
Eoin Treacy <eointreacy5@gmail.com>
2024-12-04 08:50:36 +0000

    day 4 part 2

--- day4/src/test/java/PartTwoTest.java
+++ day4/src/test/java/PartTwoTest.java
@@ -6,23 +6,23 @@ import core.InputReader;
 
 public class PartTwoTest {
 
-    // @Test
-    // public void testInputReader () {
-    //     try {
-    //         char[][] input = InputReader.readInput("src/test/resources/sample.txt");
-    //         assertEquals(PartTwo.main(input), 18);
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+    @Test
+    public void testInputReader () {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/sample.txt");
+            assertEquals(PartTwo.main(input), 9);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    // @Test
-    // public void testInput() {
-    //     try {
-    //         char[][] input = InputReader.readInput("src/test/resources/input.txt");
-    //         System.out.println(PartTwo.main(input));
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+    @Test
+    public void testInput() {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/input.txt");
+            System.out.println(PartTwo.main(input));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
