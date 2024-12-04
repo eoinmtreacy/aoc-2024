5aab96f2aaf090caedd9a8897e7c9e2c98258631
Eoin Treacy <eointreacy5@gmail.com>
2024-12-04 08:26:59 +0000

    day 4 part 1

---
+++ day4/src/test/java/PartOneTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import core.InputReader;
+
+public class PartOneTest {
+
+    @Test
+    public void testInputReader () {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/sample.txt");
+            assertEquals(PartOne.main(input), 18);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testInput() {
+        try {
+            char[][] input = InputReader.readInput("src/test/resources/input.txt");
+            System.out.println(PartOne.main(input));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ day4/src/test/java/PartTwoTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import core.InputReader;
+
+public class PartTwoTest {
+
+    // @Test
+    // public void testInputReader () {
+    //     try {
+    //         char[][] input = InputReader.readInput("src/test/resources/sample.txt");
+    //         assertEquals(PartTwo.main(input), 18);
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    // @Test
+    // public void testInput() {
+    //     try {
+    //         char[][] input = InputReader.readInput("src/test/resources/input.txt");
+    //         System.out.println(PartTwo.main(input));
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+}
