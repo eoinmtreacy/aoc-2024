bba5af3787a44dc9b54ea0bb2ce5462dce46b0ac
Eoin Treacy <eointreacy5@gmail.com>
2024-12-05 07:24:01 +0000

    day 5 part 1 updated input reader

--- core/src/main/java/core/InputReader.java
+++ core/src/main/java/core/InputReader.java
@@ -3,6 +3,7 @@ package core;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputReader {
@@ -25,6 +26,40 @@ public class InputReader {
                 result[i][j] = Integer.parseInt(parts[j]);
             }
         }
+        return result;
+    }
+
+    public static ArrayList<ArrayList<int[]>> readTwoChunks(String filePath) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        ArrayList<ArrayList<int[]>> result = new ArrayList<>();
+
+        int i = 0;
+        ArrayList<int[]> rules = new ArrayList<>();
+        while (!lines.get(i).equals("")) {
+            System.out.println(lines.get(i));
+            String[] parts = lines.get(i).split("\\|");
+            int[] partsInts = new int[parts.length];
+            for (int j = 0; j < parts.length; j++) {
+                partsInts[j] = Integer.parseInt(parts[j]);
+            }
+            rules.add(partsInts);
+            i++;
+        }
+        result.add(rules);
+        i++;
+
+        ArrayList<int[]> intLines = new ArrayList<>();
+        while (i < lines.size()) {
+            String[] parts = lines.get(i).split("\\,");
+            int[] partsInts = new int[parts.length];
+            for (int j = 0; j < parts.length; j++) {
+                partsInts[j] = Integer.parseInt(parts[j]);
+            }
+            intLines.add(partsInts);
+            i++;
+        }
+        result.add(intLines);
+
         return result;
     }
 }
