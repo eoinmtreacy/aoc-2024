ac9886eccd6ee86ae9c1357839cee902cfc9db66
Eoin Treacy <eointreacy5@gmail.com>
2024-12-06 11:18:18 +0000

    day7 init

---
+++ day7/part_one.py
@@ -0,0 +1,9 @@
+# read grid 
+with open('day7.sample', 'r') as file:
+    grid = [list(line.strip()) for line in file]
+
+# read lines
+with open('day7.sample', 'r') as file:
+    lines = [line.strip() for line in file]
+
+print(grid, lines)
