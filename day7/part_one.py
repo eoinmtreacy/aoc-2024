d58b6f06b6006c9eee640819ca87802462c85de9
eoin m treacy <eointreacy5@gmail.com>
2024-12-07 08:28:51 +0000

    day 7 part 1

--- day7/part_one.py
+++ day7/part_one.py
@@ -1,9 +1,32 @@
+from typing import List
 # read grid 
-with open('day7.sample', 'r') as file:
-    grid = [list(line.strip()) for line in file]
+# with open('day7.sample', 'r') as file:
+#     grid = [list(line.strip()) for line in file]
 
 # read lines
-with open('day7.sample', 'r') as file:
-    lines = [line.strip() for line in file]
 
-print(grid, lines)
+def getPerms(input: List[int]) -> set:
+    perms = set()
+    perms.add(input[0])
+    for num in input[1:]:
+        newPerms = set()
+        for p in perms:
+            newPerms.add(num * p)
+            newPerms.add(num + p)
+        perms = newPerms
+    return perms
+
+with open('day7.input', 'r') as file:
+    lines = [line for line in file]
+
+lines = [line.split(":") for line in lines]
+
+targets, args = [int(line[0]) for line in lines], [[int(num) for num in line[1].split()] for line in lines]
+
+result = 0 
+
+for t, a in zip(targets, args):
+    if t in getPerms(a):
+        result += t
+
+print(result)
