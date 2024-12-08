e34e1284896d7501098e10c8bcc4904505e61845
eoin m treacy <eointreacy5@gmail.com>
2024-12-08 09:25:09 +0000

    day 8 part 2

--- day8/part_two.py
+++ day8/part_two.py
@@ -1,33 +1,61 @@
-from typing import List
-# read grid 
-# with open('day7.sample', 'r') as file:
-#     grid = [list(line.strip()) for line in file]
+with open('day8.input', 'r') as file:
+    grid = [list(line.strip()) for line in file]
 
-# read lines
+map = {}
+nodes = set()
+result = 0
 
-def getPerms(input: List[int]) -> set:
-    perms = set()
-    perms.add(input[0])
-    for num in input[1:]:
-        newPerms = set()
-        for p in perms:
-            newPerms.add(num * p)
-            newPerms.add(num + p)
-            newPerms.add(int(str(p) + str(num)))
-        perms = newPerms
-    return perms
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        el = grid[r][c]
+        if el != '.':
 
-with open('day7.input', 'r') as file:
-    lines = [line for line in file]
+            if map.get(el) == None:
+                map[el] = [(r,c)]
 
-lines = [line.split(":") for line in lines]
+            else:
+                nodes.add((r,c))
+                for coord in map[el]:
+                    row, col = coord
+                    nodes.add((row, col))
 
-targets, args = [int(line[0]) for line in lines], [[int(num) for num in line[1].split()] for line in lines]
 
-result = 0 
+                    if r == row:
+                        diff = col - c
+                        inc = diff
+                        while (col - diff > -1 or c + diff < len(grid[0])):
+                            if c - diff > -1: nodes.add((r, c - diff))
+                            if col + diff < len(grid[0]): nodes.add((r, c + diff))
+                            diff += inc
 
-for t, a in zip(targets, args):
-    if t in getPerms(a):
-        result += t
 
-print(result)
+                    elif c == col:
+                        diff = row - r
+                        inc = diff
+                        while (row - diff > -1 or r + diff < len(grid)):
+                            if row - diff > -1: nodes.add((row - diff, c))
+                            if row + diff < len(grid): nodes.add((row + diff, c))
+                            diff += inc
+
+                    elif c < col:
+                        dR, dC = r - row, col - c
+                        incR, incC = dR, dC
+                        while ( ( c - dC > -1 and r + dR < len(grid) ) or ( col + dC < len(grid[0]) and row - dR > -1 ) ):
+                            if c - dC > -1 and r + dR < len(grid): nodes.add((r + dR, c - dC))
+                            if col + dC < len(grid[0]) and row - dR > -1: nodes.add((row - dR, col + dC))
+                            dR += incR
+                            dC += incC
+
+                    elif c > col:
+                        dR, dC = r - row, c - col
+                        incR, incC = dR, dC
+                        while ( (c + dC < len(grid[0]) and r + dR < len(grid)) or (col - dC > - 1 and row - dR > -1) ):
+                            if c + dC < len(grid[0]) and r + dR < len(grid): nodes.add((r + dR, c + dC))
+                            if col - dC > -1 and row - dR > -1: nodes.add((row - dR, col - dC))
+                            dR += incR
+                            dC += incC
+
+
+                map[el].append((r,c))
+
+print(len(nodes))
