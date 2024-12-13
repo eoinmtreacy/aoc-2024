da75c92d12b4df819f4f394de7ac6ee3d744229e
Eoin Treacy <eointreacy5@gmail.com>
2024-12-13 10:27:45 +0000

    [day 13 part 2] gcd

--- day13/part_two.py
+++ day13/part_two.py
@@ -10,8 +10,17 @@ def dot(m1: List[List[float]], m2: List[float]) -> List[float]:
         inter = 0
         for i in range(len(row)):
             inter += row[i] * m2[i]     
-        result.append(inter)
+        result.append(int(inter))
     return result
+
+def gcd(a: int, b: int) -> int | None:
+    n = 1
+    while min(a,b) / n > 1:
+        if max(a,b) % (min(a,b) / n) == 0:
+            return  int(min(a, b) / n)
+        n += 1
+    return 1
+
 
 result = 0
 
@@ -29,14 +38,26 @@ while i < len(input):
     C = [int(char) for char in input[i]]
     i += 1
 
+    if C[0] % gcd(v1[0], v2[0]) != 0 or C[1] % gcd(v1[1], v2[1]) != 0:
+        continue
+    
+    print("something")
     M = [v1, v2] # vector 
 
     inverse_m = [ [(1 / (v1[0] * v2[1] - v1[1] * v2[0])) * c for c in v] for v in [[v2[1], -v1[1]], [-v2[0], v1[0]]]]
-    print(dot(inverse_m, C))
     countA, countB = dot(inverse_m, C)
     print(countA, countB)
+    
+    aX, aY = v1
+    bX, bY = v2
+    targetX, targetY = C
 
-    break
+    currX = countA * aX + countB * bX
+    currY = countA * aY + countB * bY
+
+    print(gcd(aX, bX))
+
+    # break
 
 
 print(result)
