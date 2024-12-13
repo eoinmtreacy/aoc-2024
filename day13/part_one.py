dc050482c463e2ebc84c73f631e8147a353caed4
Eoin Treacy <eointreacy5@gmail.com>
2024-12-13 07:30:43 +0000

    day 13 part 1

---
+++ day13/part_one.py
@@ -0,0 +1,48 @@
+import re 
+
+with open('day13.sample', 'r') as file:
+    input = [re.findall("[0-9]+", str(line)) for line in file]
+
+result = 0
+
+i = 0
+while i < len(input):
+
+    if not input[i]:
+        i += 1
+        continue
+
+    aX, aY = [int(char) for char in input[i]]
+    i += 1
+    bX, bY = [int(char) for char in input[i]]
+    i += 1
+    targetX, targetY = [int(char) for char in input[i]]
+    i += 1
+
+    countA = 0
+    countB = min(targetX // bX, targetY // bY)
+
+    currX, currY = bX * countB, bY * countB
+
+    # print(currX, currY, targetX, targetY, aX, aY, bX, bY)
+    seen = set()
+    while (currX != targetX or currY != targetY) and (-1 < countA < 101 and countB > -1):
+        # print(currX, currY, targetX, targetY, countA, countB)
+        if currX > targetX or currY > targetY:
+            currX -= bX
+            currY -= bY
+            countB -= 1
+        else:
+            currX += aX
+            currY += aY
+            countA += 1
+        
+        if (countA, countB) in seen:
+            break
+        seen.add((countA, countB))
+
+    # print(currX, currY, targetX, targetY, countA, countB) 
+    if currX == targetX and currY == targetY and (-1 < countA < 101 or countB > -1):
+        result += countA * 3 + countB
+
+print(result)
