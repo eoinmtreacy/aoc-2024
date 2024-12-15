5d8bf7c2876e8f45b3e4e0895e1e0fe81027f921
Eoin Treacy <eointreacy5@gmail.com>
2024-12-15 14:07:06 +0000

    Refactor day 15 part two: update input file, enhance grid representation, and improve movement logic

--- day15/part_two.py
+++ day15/part_two.py
@@ -1,10 +1,15 @@
-with open('day15.input', 'r') as file:
+with open('day15.sample3', 'r') as file:
     input = file.read()
     grid, moves = input.split("\n\n")
-    grid = [[c for c in line] for line in grid.split('\n')]
-    print(len(moves.split()))
-    moves = "".join(moves.split())
-    print(len(moves))
+
+def converter(c):
+    if c == '#': return '##'
+    if c == '.': return '..'
+    if c == 'O': return '[]'
+    if c == '@': return '@.'
+
+grid = [list("".join([converter(c) for c in line])) for line in grid.split('\n')]
+moves = "".join(moves.split())
 
 ROWS: int = len(grid)
 COLS: int = len(grid[0])
@@ -18,10 +23,11 @@ dir: dict = {
 
 for i in range(ROWS):
     for j in range(COLS):
+        print(grid[i][j], end="")
         if grid[i][j] == '@':
             r, c = i, j
             grid[i][j] = '.'
-            break
+    print()
         
 for m in moves:
     dr, dc = dir[m]
@@ -35,18 +41,28 @@ for m in moves:
         c += dc
         continue
 
-    while grid[pr][pc] != '#' and grid[pr][pc] != '.':
-        pr += dr
-        pc += dc
-    if grid[pr][pc] == '.':
-        grid[pr][pc], grid[r + dr][c + dc] = grid[r + dr][c + dc], grid[pr][pc]
-        r += dr
-        c += dc
+    # horizontal movement
+    if dr == 0:
+        while grid[pr][pc] != '#' and grid[pr][pc] != '.':
+            pr += dr
+            pc += dc
+        
+        if grid[pr][pc] == '.':
+            if dc < 0: # pushing left
+                grid[pr][pc : c] = grid[pr][pc + 1: c + 1]
+            else:
+                grid[pr][c + dc + 1 : pc + 1] = grid[pr][c + dc : pc]
+                grid[pr][c + dc] = '.'
+            r += dr
+            c += dc
+    # break
 
 result = 0
 for r in range(ROWS):
     for c in range(COLS):
+        print(grid[r][c], end='')
         if grid[r][c] == 'O':
             result += c + 100 * r
+    print()
 
 print(result)
